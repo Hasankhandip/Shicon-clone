8c0f0bd08cd735a7e50e6ddff83efbee365497d9
Dip <hasankhandip@gmail.com>
2021-10-31 20:19:50 +0600

    images adding

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,10 @@ $(window).on("scroll", function(){
       }
 });
 
+// unvail lazy loader start
+// $("img").unveil(200, function() {
+//     $(this).load(function() {
+//       this.style.opacity = 1;
+//     });
+//   });   
+// unvail lazy loader end
