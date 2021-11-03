e6d55ad536c2e4e9193437a2c04f58f1ef036b1a
Dip <hasankhandip@gmail.com>
2021-11-03 23:29:22 +0600

    some js edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,27 @@
 new WOW().init();
 // wow.js end
 
+// unvail lazy loader start
+$("img").unveil(200, function() {
+  $(this).load(function() {
+    this.style.opacity = 1;
+  });
+});   
+// unvail lazy loader end
+
+// scroll-top btn start
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 300) {
+    $(".scroll-top").addClass("active");
+  } else {
+    $(".scroll-top").removeClass("active");
+  }
+});
+
+$(".scroll-top").on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
+});
+// scroll-top btn end
 
 // odometer js start
 $(window).on("scroll", function(){
@@ -18,11 +39,31 @@ $(window).on("scroll", function(){
 });
 // odometer js end
 
+// header-navbar js start
+$(window).on("scroll", function(){
+  if ($(this).scrollTop() > 85 ) {
+   $(".header-navbar").addClass("fixed");
+   $(".header-logo").addClass("remove");
+   $(".header-logo-fixed").addClass("active");
+   $(".nav-link").addClass("active");
+   $(".active-nav").addClass("active");
+   $(".header-serch-bar").addClass("active");
+  } else {
+    $(".header-navbar").removeClass("fixed");
+    $(".header-logo").removeClass("remove");
+    $(".header-logo-fixed").removeClass("active");
+    $(".nav-link").removeClass("active");
+    $(".active-nav").removeClass("active");
+    $(".header-serch-bar").removeClass("active");
+  }
+});
+// header-navbar js end
 
-// unvail lazy loader start
-$("img").unveil(200, function() {
-    $(this).load(function() {
-      this.style.opacity = 1;
-    });
-  });   
-// unvail lazy loader end
+//  header-search-bar js start
+$(".header-serch-bar").on("click",function(){
+  $(".header-search-input-item").addClass("active");
+});
+$(".banner-section").on("click",function(){
+  $(".header-search-input-item").removeClass("active");
+});
+//  header-search-bar js end
